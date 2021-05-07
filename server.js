16cd849edf9c1bcc1c829e5874d633b237bf9cce
supun <supunmadushanka19980822@gmail.com>
2021-05-07 15:47:28 +0530

    homedone7

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
-const app = express();
 app.use(requireHTTPS);
 
 app.use(express.static('./dist/pr1'));
